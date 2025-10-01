d7c417f0f1e957ac454bb6e692f5736b8dad834e
Vincent <antoniod877@gmail.com>
2025-10-01 20:01:18 +0200

    Optimize activite fetch with join

--- src/main/java/friendsofmine/m2/repositories/ActiviteRepository.java
+++ src/main/java/friendsofmine/m2/repositories/ActiviteRepository.java
@@ -1,8 +1,13 @@
 package friendsofmine.m2.repositories;
 
 import friendsofmine.m2.domain.Activite;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ActiviteRepository extends CrudRepository<Activite, Long> {
+import java.util.List;
 
+public interface ActiviteRepository extends JpaRepository<Activite, Long> {
+
+    @Query("select distinct a from Activite a join fetch a.responsable order by lower(a.titre)")
+    List<Activite> findAllWithResponsableOrderByTitre();
 }
