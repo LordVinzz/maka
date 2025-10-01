41571f2ad4685272d8545baa05d88ef755f23746
Vincent DOMINGUEZ <antoniod877@gmail.com>
2025-10-01 22:52:15 +0200

    m

--- src/main/java/friendsofmine/m2/DataLoader.java
+++ src/main/java/friendsofmine/m2/DataLoader.java
@@ -4,6 +4,7 @@ import friendsofmine.m2.domain.Activite;
 import friendsofmine.m2.domain.Utilisateur;
 import friendsofmine.m2.services.ActiviteService;
 import friendsofmine.m2.services.UtilisateurService;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -15,10 +16,29 @@ public class DataLoader implements ApplicationRunner {
     private final UtilisateurService utilisateurService;
     private final ActiviteService activiteService;
 
+    @Getter
     private final Utilisateur thom = new Utilisateur("Yorke", "Thom", "thom@rh.com", "M");
+    @Getter
     private final Utilisateur ed = new Utilisateur("Obrien", "Ed", "ed@rh.com", "M");
+    @Getter
     private final Utilisateur karen = new Utilisateur("Orzolek", "Karen", "karen@yyy.com", "F");
+    @Getter
     private final Utilisateur julian = new Utilisateur("Casablancas", "Julian", "jc@ts.com", "M");
+
+    @Getter
+    private final Activite guitare = new Activite("Guitare", "Matériel non fourni", thom);
+    @Getter
+    private final Activite muscu = new Activite("Muscu", "Créneau réservé le mardi", ed);
+    @Getter
+    private final Activite poker = new Activite("Poker", "Petite blind à 1 euro", karen);
+    @Getter
+    private final Activite pingpong = new Activite("Ping Pong", "Matériel non fourni", julian);
+    @Getter
+    private final Activite jogging = new Activite("Jogging", "Tous les midis", ed);
+    @Getter
+    private final Activite philo = new Activite("Philo", "Le club des admirateurs de Socrate", thom);
+    @Getter
+    private final Activite procrastination = new Activite("Procrastination", "On verra demain", thom);
 
     @Autowired
     public DataLoader(ActiviteService activiteService, UtilisateurService utilisateurService) {
@@ -26,73 +46,34 @@ public class DataLoader implements ApplicationRunner {
         this.activiteService = activiteService;
     }
 
-    public Utilisateur getThom() {
-        return thom;
-    }
-
-    public Utilisateur getEd() {
-        return ed;
-    }
-
-    public Utilisateur getKaren() {
-        return karen;
-    }
-
-    public Utilisateur getJulian() {
-        return julian;
-    }
-
-    public Activite getGuitare() {
-        return new Activite("Guitare", "Matériel non fourni", getThom());
-    }
-
-    public Activite getMuscu() {
-        return new Activite("Muscu", "Créneau réservé le mardi", getEd());
-    }
-
-    public Activite getPoker() {
-        return new Activite("Poker", "Petite blind à 1 euro", getKaren());
-    }
-
-    public Activite getPingpong() {
-        return new Activite("Ping Pong", "Matériel non fourni", getJulian());
-    }
-
-    public Activite getJogging() {
-        return new Activite("Jogging", "Tous les midis", getEd());
-    }
-
-    public Activite getPhilo() {
-        return new Activite("Philo", "Le club des admirateurs de Socrate", getThom());
-    }
-
-    public Activite getProcrastination() {
-        return new Activite("Procrastination", "On verra demain", getThom());
-    }
-
     public void initUtilisateurs() {
-        utilisateurService.saveUtilisateur(getThom());
-        utilisateurService.saveUtilisateur(getEd());
-        utilisateurService.saveUtilisateur(getKaren());
-        utilisateurService.saveUtilisateur(getJulian());
+        // Les utilisateurs sont déjà instanciés via les attributs de la classe.
+        // Cette méthode permet de conserver la sémantique historique qui
+        // sépare la préparation des données de leur persistance effective.
     }
 
     public void initActivites() {
-        activiteService.saveActivite(getGuitare());
-        activiteService.saveActivite(getMuscu());
-        activiteService.saveActivite(getPoker());
-        activiteService.saveActivite(getPingpong());
-        activiteService.saveActivite(getJogging());
-        activiteService.saveActivite(getPhilo());
-        activiteService.saveActivite(getProcrastination());
+        thom.addActivite(guitare);
+        ed.addActivite(muscu);
+        karen.addActivite(poker);
+        julian.addActivite(pingpong);
+        ed.addActivite(jogging);
+        thom.addActivite(philo);
+        thom.addActivite(procrastination);
     }
 
     @Override
     public void run(ApplicationArguments args) {
+
         initUtilisateurs();
         initActivites();
+        saveUtilisateursAndActivites();
     }
 
     public void saveUtilisateursAndActivites() {
+        utilisateurService.saveUtilisateur(thom);
+        utilisateurService.saveUtilisateur(ed);
+        utilisateurService.saveUtilisateur(karen);
+        utilisateurService.saveUtilisateur(julian);
     }
 }
